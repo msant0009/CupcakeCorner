a6acd705fccddeb5e73a317dcc54743021000dba
Mark Santoro <mark_santoro@comcast.net>
2024-12-03 10:28:07 -0800

    added json data download code

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -38,7 +38,20 @@ struct ContentView: View {
     }
     
     func loadData() async {
+        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
+            print("Invalid URL")
+            return
+        }
         
+        do {
+            let(data, _) = try await URLSession.shared.data(from: url)
+            
+            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+                results = decodedResponse.results
+            }
+        } catch {
+            print("Invalid data")
+        }
     }
 }
 
