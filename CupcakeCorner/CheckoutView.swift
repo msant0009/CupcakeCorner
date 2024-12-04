06d43e98347213eff233ca8e2968fdc3b82ac4d0
Mark Santoro <mark_santoro@comcast.net>
2024-12-04 09:34:28 -0800

    add encode/decode for sending the order plus error checking.
    App complete!

--- CupcakeCorner/CheckoutView.swift
+++ CupcakeCorner/CheckoutView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct CheckoutView: View {
+    @State private var confirmationMessage = ""
+    @State private var showingConfirmation = false
+    
     var order: Order
     
     var body: some View {
@@ -37,11 +40,37 @@ struct CheckoutView: View {
         .navigationTitle("Check Out")
         .navigationBarTitleDisplayMode(.inline)
         .scrollBounceBehavior(.basedOnSize)
+        .alert("Thank you!", isPresented: $showingConfirmation) {
+            Button("OK"){}
+        } message: {
+            Text(confirmationMessage)
+        }
         
         
     }
     
     func placeOrder() async {
+        guard let encoded = try? JSONEncoder().encode(order) else {
+            print("Failed to encode order")
+            return
+        }
+        
+        let url = URL(string: "https://reqres.in/api/cupcakes")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "Post"
+        
+        do {
+            let (data, _) = try await URLSession.shared.upload(for: request, from: encoded)
+            
+            let decodedOrder = try JSONDecoder().decode(Order.self, from: data)
+            confirmationMessage = "Your order for \(decodedOrder.quantity)x \(Order.types[decodedOrder.type].lowercased()) cupcakes is on its way!"
+            showingConfirmation = true
+            
+        } catch {
+            print("Checkout Failed! \(error.localizedDescription)")
+        }
+        
         
     }
 }
